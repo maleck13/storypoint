7d9f3be7936a47796913ecbfef56efaa75af0582
Jameel Briones <jbriones@redhat.com>
2018-07-06 09:45:41 +0100

    create start session component

--- react-web/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class Home extends React.Component {
-  render() {
-    return( 
-      <div className="homepage"></div>
-    );
-  }
-}
---
+++ react-web/src/components/Session.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Session extends React.Component {
+
+  render() {
+    return( 
+      <div className="session">
+        <header className="text-center">
+          <h1> Session </h1>
+        </header>
+      </div>
+    );
+  }
+}
