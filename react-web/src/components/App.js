0750444f714c09b29a800823233cf99c0948f1ce
John Roche <jroche@redhat.com>
2018-07-09 12:42:23 +0100

    Use default settings with new sass watch scripts

--- react-web/src/components/App.js
+++ react-web/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
 import NavBar from './NavBar';
 import { Home } from './Home';
 import { Redirect, Route, Switch } from "react-router-dom";
